5f1ddf7d3ce2412115790a80db027889d2c68a3f
Reepu95013 <reepushah95013@gmail.com>
2025-10-27 06:22:51 +0530

    updated file

--- src/components/SecondLayout.js
+++ src/components/SecondLayout.js
@@ -1,60 +1,73 @@
-import { View, Text, SafeAreaView, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
-import React, { memo } from 'react'
-import { useColorTheme } from '../context/ThemeContext'
-import Icon from 'react-native-vector-icons/MaterialIcons'
-import { useNavigation } from '@react-navigation/native'
-import createStyles from '../constants/styles'
-
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import React, { memo } from 'react';
+import { useColorTheme } from '../context/ThemeContext';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation } from '@react-navigation/native';
+import createStyles from '../constants/styles';
 
 const SecondLayout = ({ children, screenName, CustomHeader, CustomBottom }) => {
-    const { themeColor, fontFamily } = useColorTheme();
-    const navigation = useNavigation();
-    const styles = createStyles(themeColor, fontFamily);
+  const { themeColor, fontFamily } = useColorTheme();
+  const navigation = useNavigation();
+  const styles = createStyles(themeColor, fontFamily);
+ 
+  return (
+    <SafeAreaView
+      style={[
+        customstyles.container,
+        { backgroundColor: themeColor.background },
+      ]}
+    >
+      <>
+        {!CustomHeader && (
+          <View
+            style={{
+              marginTop: 60,
+              marginBottom: 12,
+              marginHorizontal: 16,
+              flexDirection: 'row',
+              gap: 16,
+            }}
+          >
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Icon
+                name="chevron-left"
+                size={32}
+                color={themeColor.iconSecondary}
+              />
+            </TouchableOpacity>
+            <Text style={styles.header}>{screenName || ''}</Text>
+          </View>
+        )}
+      </>
+      <FlatList
+        data={[1]}
+        renderItem={() => <View style={customstyles.content}>{children}</View>}
+        ListHeaderComponent={ CustomHeader && <CustomHeader/>}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 150 }}
+      />
 
-
-    const headerRender = () => (
-        <>
-            {CustomHeader ? (<CustomHeader />) :
-                (<View style={{ marginTop: 60, marginBottom: 12, marginHorizontal: 16, flexDirection: 'row', gap: 16 }}>
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <Icon name='chevron-left' size={32} color={themeColor.iconSecondary} />
-                    </TouchableOpacity>
-                    <Text style={styles.header}>{screenName || ''}</Text>
-                </View>)
-            }
-        </>
-
-    )
-    return (
-        <SafeAreaView style={[customstyles.container, { backgroundColor: themeColor.background }]}>
-
-            <FlatList
-                data={[1]}
-                renderItem={() => (
-                    <View style={customstyles.content}>
-                        {children}
-                    </View>
-                )}
-                ListHeaderComponent={headerRender}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 150, }}
-            />
-
-            {CustomBottom && <CustomBottom />}
-        </SafeAreaView>
-    )
-}
-
+      {CustomBottom && <CustomBottom />}
+    </SafeAreaView>
+  );
+};
 
 const customstyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'red',
-    },
-    content: {
-        flex: 1,
-        marginHorizontal: 12
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: 12,
+  },
 });
 
 export default memo(SecondLayout);
--- src/screens/admin/adminAuth/AdminRegisterScreen.js
+++ src/screens/admin/adminAuth/AdminRegisterScreen.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, ScrollView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { useColorTheme } from "../../../context/ThemeContext";
-import createStyles from "../../../constants/styles";
-import CustomInputText from "../../../components/CustomInputText";
-import { timing } from "react-native/types_generated/Libraries/Animated/AnimatedExports";
-import CustomButton from "../../../components/CustomButton";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import React, { useState } from 'react';
+import SecondLayout from '../../../components/SecondLayout';
+import { useTranslation } from 'react-i18next';
+import { useColorTheme } from '../../../context/ThemeContext';
+import createStyles from '../../../constants/styles';
+import CustomInputText from '../../../components/CustomInputText';
+import CustomButton from '../../../components/CustomButton';
 import Icon from 'react-native-vector-icons/AntDesign';
-import SecondLayout from "../../../components/SecondLayout";
 
 const AdminRegisterScreen = () => {
   const { t } = useTranslation();
@@ -17,10 +23,29 @@ const AdminRegisterScreen = () => {
   const [step, setStep] = useState(1);
 
   // States for forms
-  const [accountInfo, setAccountInfo] = useState({ name: "", email: "", username: "", mobile: "", opt: "", password: "" });
-  const [salonDetails, setSalonDetails] = useState({ salonName: "", category: "", WorkingTime: "", address: "", city: "", pinCode: "", salonContact: "", texID: "" });
-  const [verification, setVerification] = useState({ idProof: "", licence: "" });
-  const [bankDetails, setBankDetails] = useState({ accountNo: "", ifsc: "" });
+  const [accountInfo, setAccountInfo] = useState({
+    name: '',
+    email: '',
+    username: '',
+    mobile: '',
+    opt: '',
+    password: '',
+  });
+  const [salonDetails, setSalonDetails] = useState({
+    salonName: '',
+    category: '',
+    WorkingTime: '',
+    address: '',
+    city: '',
+    pinCode: '',
+    salonContact: '',
+    texID: '',
+  });
+  const [verification, setVerification] = useState({
+    idProof: '',
+    licence: '',
+  });
+  const [bankDetails, setBankDetails] = useState({ accountNo: '', ifsc: '' });
 
   const handleNext = () => {
     if (step < 4) setStep(step + 1);
@@ -30,7 +55,7 @@ const AdminRegisterScreen = () => {
   };
   const handleSubmit = () => {
     console.log({ accountInfo, salonDetails, verification, bankDetails });
-    alert("Registration Completed!");
+    // alert("Registration Completed!");
   };
 
   // Render step content
@@ -39,13 +64,14 @@ const AdminRegisterScreen = () => {
       case 1:
         return (
           <>
-
             <CustomInputText
               label={t('full_name')}
               placeholder={t('full_name')}
               required
               value={accountInfo.name}
-              onChangeText={(text) => setAccountInfo({ ...accountInfo, name: text })}
+              onChangeText={text =>
+                setAccountInfo({ ...accountInfo, name: text })
+              }
             />
 
             <CustomInputText
@@ -53,7 +79,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('email')}
               required
               value={accountInfo.email}
-              onChangeText={(text) => setAccountInfo({ ...accountInfo, email: text })}
+              onChangeText={text =>
+                setAccountInfo({ ...accountInfo, email: text })
+              }
               keyboardType="email-address"
             />
 
@@ -62,7 +90,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('username')}
               required
               value={accountInfo.username}
-              onChangeText={(text) => setAccountInfo({ ...accountInfo, username: text })}
+              onChangeText={text =>
+                setAccountInfo({ ...accountInfo, username: text })
+              }
               maxLength={10}
             />
 
@@ -71,19 +101,30 @@ const AdminRegisterScreen = () => {
               placeholder={t('mobile')}
               required
               value={accountInfo.mobile}
-              onChangeText={(text) => setAccountInfo({ ...accountInfo, mobile: text })}
+              onChangeText={text =>
+                setAccountInfo({ ...accountInfo, mobile: text })
+              }
               maxLength={10}
               keyboardType="phone-pad"
             />
 
-            <Text style={[commonStyles.largeText, { textAlign: 'right', textDecorationLine: 'underline' }]}>{t('get_otp')}</Text>
+            <Text
+              style={[
+                commonStyles.largeText,
+                { textAlign: 'right', textDecorationLine: 'underline' },
+              ]}
+            >
+              {t('get_otp')}
+            </Text>
 
             <CustomInputText
               label={t('otp')}
               placeholder={t('otp')}
               required
               value={accountInfo.opt}
-              onChangeText={(text) => setAccountInfo({ ...accountInfo, opt: text })}
+              onChangeText={text =>
+                setAccountInfo({ ...accountInfo, opt: text })
+              }
               maxLength={6}
               keyboardType="phone-pad"
             />
@@ -93,7 +134,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('password')}
               required
               value={accountInfo.password}
-              onChangeText={(text) => setAccountInfo({ ...accountInfo, password: text })}
+              onChangeText={text =>
+                setAccountInfo({ ...accountInfo, password: text })
+              }
               secureTextEntry
             />
           </>
@@ -101,20 +144,23 @@ const AdminRegisterScreen = () => {
       case 2:
         return (
           <>
-
             <CustomInputText
               label={t('salon_name')}
               placeholder={t('salon_name')}
               required
               value={salonDetails.salonName}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, salonName: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, salonName: text })
+              }
             />
             <CustomInputText
               label={t('phone')}
               placeholder={t('phone')}
               required
               value={salonDetails.salonContact}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, salonContact: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, salonContact: text })
+              }
               keyboardType="phone-pad"
               maxLength={6}
             />
@@ -124,7 +170,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('category')}
               required
               value={salonDetails.category}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, category: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, category: text })
+              }
             />
 
             <CustomInputText
@@ -132,7 +180,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('working_time')}
               required
               value={salonDetails.WorkingTime}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, WorkingTime: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, WorkingTime: text })
+              }
             />
 
             <CustomInputText
@@ -140,12 +190,13 @@ const AdminRegisterScreen = () => {
               placeholder={t('address')}
               required
               value={salonDetails.address}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, address: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, address: text })
+              }
               numberOfLines={4}
               multiline={true}
               textAlignVertical={'top'}
               style={{ minHeight: 100 }}
-
             />
 
             <CustomInputText
@@ -153,7 +204,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('city')}
               required
               value={salonDetails.city}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, city: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, city: text })
+              }
             />
 
             <CustomInputText
@@ -161,7 +214,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('pin_code')}
               required
               value={salonDetails.pinCode}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, pinCode: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, pinCode: text })
+              }
               keyboardType="phone-pad"
               maxLength={6}
             />
@@ -171,10 +226,11 @@ const AdminRegisterScreen = () => {
               placeholder={t('tex_number')}
               required
               value={salonDetails.texID}
-              onChangeText={(text) => setSalonDetails({ ...salonDetails, texID: text })}
+              onChangeText={text =>
+                setSalonDetails({ ...salonDetails, texID: text })
+              }
               maxLength={20}
             />
-
           </>
         );
       case 3:
@@ -183,37 +239,69 @@ const AdminRegisterScreen = () => {
             <View style={{ gap: 40 }}>
               <View style={{ gap: 12 }}>
                 <Text style={commonStyles.header}>Upload Adhaar Card</Text>
-                <View style={{ borderWidth: 1, borderRadius: 10, borderColor: 'white', height: 150, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+                <View
+                  style={{
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    borderColor: 'white',
+                    height: 150,
+                    padding: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
                   <Icon name={'upload'} size={40} color={themeColor.primary} />
-                  <CustomButton
-                    label={t('upload')}
-                    style={{ width: '80%' }}
-                  />
-
+                  <CustomButton label={t('upload')} style={{ width: '80%' }} />
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
                   <Text style={commonStyles.largeText}>adhharcar.png</Text>
-                  <Text style={[commonStyles.largeText, [{ textDecorationLine: 'underline' }]]}>{t('view')}</Text>
-
+                  <Text
+                    style={[
+                      commonStyles.largeText,
+                      [{ textDecorationLine: 'underline' }],
+                    ]}
+                  >
+                    {t('view')}
+                  </Text>
                 </View>
-
               </View>
               <View style={{ gap: 12 }}>
                 <Text style={commonStyles.header}>Upload Shop License</Text>
-                <View style={{ borderWidth: 1, borderRadius: 10, borderColor: 'white', height: 150, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+                <View
+                  style={{
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    borderColor: 'white',
+                    height: 150,
+                    padding: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
                   <Icon name={'upload'} size={40} color={themeColor.primary} />
-                  <CustomButton
-                    label={t('upload')}
-                    style={{ width: '80%' }}
-                  />
-
+                  <CustomButton label={t('upload')} style={{ width: '80%' }} />
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
                   <Text style={commonStyles.largeText}>License.png</Text>
-                  <Text style={[commonStyles.largeText, [{ textDecorationLine: 'underline' }]]}>{t('view')}</Text>
-
+                  <Text
+                    style={[
+                      commonStyles.largeText,
+                      [{ textDecorationLine: 'underline' }],
+                    ]}
+                  >
+                    {t('view')}
+                  </Text>
                 </View>
-
               </View>
             </View>
           </>
@@ -221,13 +309,14 @@ const AdminRegisterScreen = () => {
       case 4:
         return (
           <>
-
             <CustomInputText
               label={t('account_number')}
               placeholder={t('account_number')}
               required
               value={bankDetails.accountNo}
-              onChangeText={(text) => setBankDetails({ ...bankDetails, accountNo: text })}
+              onChangeText={text =>
+                setBankDetails({ ...bankDetails, accountNo: text })
+              }
             />
 
             <CustomInputText
@@ -235,7 +324,9 @@ const AdminRegisterScreen = () => {
               placeholder={t('ifsc_code')}
               required
               value={bankDetails.ifsc}
-              onChangeText={(text) => setBankDetails({ ...bankDetails, ifsc: text })}
+              onChangeText={text =>
+                setBankDetails({ ...bankDetails, ifsc: text })
+              }
             />
           </>
         );
@@ -248,73 +339,87 @@ const AdminRegisterScreen = () => {
   const StepIndicator = () => {
     const progress = (step / 4) * 100; // percentage
     return (
-      <View style={[styles.progressContainer, { backgroundColor: themeColor.secondaryBackground }]}>
-        <View style={[styles.progressBar, { width: `${progress}%`, backgroundColor: themeColor.primary }]} />
+      <View
+        style={[
+          styles.progressContainer,
+          { backgroundColor: themeColor.secondaryBackground },
+        ]}
+      >
+        <View
+          style={[
+            styles.progressBar,
+            { width: `${progress}%`, backgroundColor: themeColor.primary },
+          ]}
+        />
       </View>
     );
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: themeColor.background }} edges={["top"]}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={[styles.container]}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          {/* <View style={{ height: '100%' }}> */}
-          <SecondLayout screenName={'regi'} />
-          {/* Progress Bar */}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SecondLayout screenName={'register'}>
           <StepIndicator />
-          <Text style={[commonStyles.largeText, { textAlign: 'center' }]}>{t('step')} {step} / 4</Text>
-
+          <Text style={[commonStyles.largeText, { textAlign: 'center' }]}>
+            {t('step')} {step} / 4
+          </Text>
           {/* Centered Content */}
           <View style={styles.contentWrapper}>
-            <Text style={[commonStyles.header, { marginBottom: 20, textAlign: 'center' }]}>
+            <Text
+              style={[
+                commonStyles.header,
+                { marginBottom: 20, textAlign: 'center' },
+              ]}
+            >
               {step === 1
                 ? t('account_information')
                 : step === 2
-                  ? t('salon_details')
-                  : step === 3
-                    ? t('documents')
-                    : t('bank_details')}
+                ? t('salon_details')
+                : step === 3
+                ? t('documents')
+                : t('bank_details')}
             </Text>
-            <ScrollView showsVerticalScrollIndicator={false} >
-              {renderStep()}
 
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              keyboardShouldPersistTaps="handled"
+            >
+              {renderStep()}
               {/* Navigation */}
               <View style={styles.buttonRow}>
                 {step > 1 && (
-
                   <CustomButton
                     label={t('back')}
-                    style={{ width: '48%', backgroundColor: themeColor.surface, }}
+                    style={{
+                      width: '48%',
+                      backgroundColor: themeColor.surface,
+                    }}
                     textStyle={{ color: themeColor.surfacetText }}
                     onPress={handleBack}
                   />
-
                 )}
                 {step < 4 ? (
                   <CustomButton
                     label={t('next')}
-                    style={{ width: step == 1 ? '100%' : '48%', }}
+                    style={{ width: step == 1 ? '100%' : '48%' }}
                     onPress={handleNext}
                   />
-
                 ) : (
                   <CustomButton
                     label={t('submit')}
                     style={{ width: '48%' }}
                     onPress={handleSubmit}
                   />
-
                 )}
               </View>
             </ScrollView>
           </View>
-          <SecondLayout />
-          {/* </View> */}
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    </SafeAreaView >
+        </SecondLayout>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -329,44 +434,40 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginHorizontal: 20,
     marginTop: 20,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   progressBar: {
-    height: "100%",
-    backgroundColor: "#007bff",
+    height: '100%',
+    backgroundColor: '#007bff',
   },
   stepText: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 14,
-    fontWeight: "600",
+    fontWeight: '600',
     marginVertical: 10,
   },
   contentWrapper: {
     flex: 1,
-    justifyContent: "center", // Center vertically
+    justifyContent: 'center', // Center vertically
     paddingHorizontal: 20,
   },
   title: {
     fontSize: 22,
-    fontWeight: "700",
+    fontWeight: '700',
     marginBottom: 20,
-    textAlign: "center",
-    color: "#333",
+    textAlign: 'center',
+    color: '#333',
   },
   input: {
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     padding: 12,
     borderRadius: 10,
     marginBottom: 15,
   },
   buttonRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 20,
   },
-
-
-
-
 });
